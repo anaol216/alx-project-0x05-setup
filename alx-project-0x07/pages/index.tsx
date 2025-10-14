742e30b07171b4d8b38879eb55abe613e0739aa4
anaol216 <atinafuanaol@gmail.com>
2025-10-14 11:56:27 +0300

    new file:   alx-project-0x07/components/common/ImageCard.tsx modified:   alx-project-0x07/interfaces/index.ts modified:   alx-project-0x07/pages/index.tsx

--- alx-project-0x07/pages/index.tsx
+++ alx-project-0x07/pages/index.tsx
@@ -1,20 +1,23 @@
-
-import React, { useState } from "react";
-
-
-export interface ReactComponentProps {
-    children: React.ReactNode;
-}
+import ImageCard from "@/components/common/ImageCard";
+import { ImageProps } from "@/interfaces";
+import { useState } from "react";
 
 
 
 const Home: React.FC = () => {
+  const [prompt, setPrompt] = useState<string>("");
+  const [imageUrl, setImageUrl] = useState<string>("");
+  const [generatedImages, setGeneratedImages] = useState<ImageProps[]>(
+    []
+  );
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+
   const handleGenerateImage = async () => {
-    console.log("Generating Images")
+    console.log("Generating Images");
   };
 
   return (
-    
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
       <div className="flex flex-col items-center">
         <h1 className="text-4xl font-bold mb-2">Image Generation App</h1>
@@ -25,6 +28,8 @@ const Home: React.FC = () => {
         <div className="w-full max-w-md">
           <input
             type="text"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
             placeholder="Enter your prompt here..."
             className="w-full p-3 border border-gray-300 rounded-lg mb-4"
           />
@@ -32,11 +37,16 @@ const Home: React.FC = () => {
             onClick={handleGenerateImage}
             className="w-full p-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition duration-200"
           >
+            {/* {
+              isLoading ? "Loading..." : "Generate Image"
+            } */}
             Generate Image
           </button>
         </div>
+
+        {imageUrl && <ImageCard action={() => setImageUrl(imageUrl)} imageUrl={imageUrl} prompt={prompt} />}
       </div>
-     </div>
+    </div>
   );
 };
 
